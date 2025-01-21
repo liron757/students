1551cbbe8ff2c6e3460ec0b5e13aaafae186ccce
Liron Shriki <liron757@gmail.com>
2025-01-21 18:17:25 +0200

    added user details

--- app/src/main/java/com/example/students/Student.kt
+++ app/src/main/java/com/example/students/Student.kt
@@ -1,5 +1,7 @@
 package com.example.students
 
+import androidx.activity.result.ActivityResult
+
 data class Student(
     val id: String,
     val name: String,
@@ -7,3 +9,26 @@ data class Student(
     val address: String,
     val isChecked: Boolean = false
 )
+
+fun getUpdatedStudent(result: ActivityResult): Pair<Student, Int> {
+    val data = result.data
+
+    val position = data?.getIntExtra("POSITION", -1) ?: -1
+    val updatedStudentId = data?.getStringExtra("UPDATED_STUDENT_ID") ?: ""
+    val updatedStudentName = data?.getStringExtra("UPDATED_STUDENT_NAME") ?: ""
+    val updatedStudentPhone = data?.getStringExtra("UPDATED_STUDENT_PHONE") ?: ""
+    val updatedStudentAddress = data?.getStringExtra("UPDATED_STUDENT_ADDRESS") ?: ""
+    val updatedStudentChecked =
+        data?.getBooleanExtra("UPDATED_STUDENT_CHECKED", false) ?: false
+
+    return Pair(
+        Student(
+            updatedStudentId,
+            updatedStudentName,
+            updatedStudentPhone,
+            updatedStudentAddress,
+            updatedStudentChecked
+        ),
+        position
+    )
+}
